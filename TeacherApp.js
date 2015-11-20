242a6dd03bd80644a5d67ba8bf6b0249b9e8ac9a
Dezmon Fernandez <defernan@mines.edu>
2015-11-19 23:52:12 -0700

    editing done with the exception of age, oh and delete too

--- TeacherApp.js
+++ TeacherApp.js
@@ -1,8 +1,40 @@
 
-var teacherApp = angular.module('teacherList')....
+var teacherApp = angular.module('teacherApp', []);
 
-var ListController = function (){
-	
+teacherApp.controller('ListController', ['$scope', function($scope){
 
 
-};
+	$scope.student = {name: 'willis', grade:'45'};
+	$scope.student2 = {name: 'willis', grade:'45'};
+	$scope.student3 = {name: 'willis', grade:'45'};
+	$scope.studentList= [$scope.student, $scope.student2, $scope.student3];
+	$scope.studentCopy = {};
+	$scope.editing = {};
+	$scope.editing[0] = false;
+	$scope.editing[1] = false;
+	$scope.editing[2] = false;
+	$scope.edit = function(student, index){
+		console.log(index);
+		$scope.editing[index] = true;
+		$scope.studentCopy = angular.copy(student)
+	};
+	$scope.done = function(index){
+		$scope.editing[index] = false;
+	}
+
+	$scope.delete = function(index){
+		console.log(index);
+		$scope.studentList.splice(index,1);
+	};
+
+	$scope.enterPressed = function (index, keyPressed){
+		console.log(keyPressed);
+		if(keyPressed.keyCode == 13){
+			$scope.editing[index] = false;
+		}
+	}
+	//try with directives
+	// teacherApp.directive('Enter', function(){
+	// 	return function()
+	// })
+}]);
